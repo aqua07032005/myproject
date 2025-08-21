7f43dbe11335bff66a7a8515cf7d31bf942104c0
aqua07032005 <aqua07032005@gmail.com>
2025-08-21 14:12:35 +0530

    Added files via upload
    It's  the code for ATM Simulation System

---
+++ Conn.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","root");
+            s =c.createStatement();
+
+
+
+        }catch(Exception a){
+            System.out.println(a);
+        }
+    }
+}
+
